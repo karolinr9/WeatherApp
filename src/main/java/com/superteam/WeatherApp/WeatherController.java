0ada5b421b3a418da5660ce006c5d09f8d9a0d51
karolinr9 <klrykkelid@outlook.com>
2018-09-19 13:15:00 +0200

    small fixes

--- src/main/java/com/superteam/WeatherApp/WeatherController.java
+++ src/main/java/com/superteam/WeatherApp/WeatherController.java
@@ -18,7 +18,11 @@ public class WeatherController {
         stringBuilder.append("<html>");
         stringBuilder.append("<head><title>Weather App</title><style>#map {height: 400px;  width: 100%;  }</style></head>");
         stringBuilder.append("<body>");
-        stringBuilder.append("<div id=\"map\"></div><script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});var marker = new google.maps.Marker({position: uluru, map: map});}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
+        stringBuilder.append("<div id=\"map\"></div>" +
+                "<script>function initMap() {var uluru = {lat: -25.344, lng: 131.036};" +
+                "var map = new google.maps.Map(document.getElementById('map'), {zoom: 4, center: uluru});" +
+                "var marker = new google.maps.Marker({position: uluru, map: map});}</script>" +
+                "<script async defer src=\"https://maps.googleapis.com/maps/api/js?key=" + GOOGLE_API_KEY + "&callback=initMap\"></script>");
 
         stringBuilder.append("<h1>Winter is coming</h1>");
 
