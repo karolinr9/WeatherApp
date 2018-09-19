223c3479edd50ac8b8618b7f9dec1bf7c2f2076d
karolinr9 <klrykkelid@outlook.com>
2018-09-19 15:12:03 +0200

    small change

--- src/main/java/com/superteam/WeatherApp/WeatherHandler.java
+++ src/main/java/com/superteam/WeatherApp/WeatherHandler.java
@@ -1,11 +1,16 @@
 package com.superteam.WeatherApp;
 
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+//import java.net.URLConnection;
+
 public class WeatherHandler {
 
 	public WeatherHandler(){
 
 	}
-
 
 
 }
