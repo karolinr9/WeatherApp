c78278c5c0a8f03f80fa77a01b77ea36628f33ae
Kenneth Storli <kenneth.storli@gmail.com>
2018-09-21 14:00:20 +0200

    Changed handler permissions

--- src/main/java/com/superteam/WeatherApp/WeatherHandler.java
+++ src/main/java/com/superteam/WeatherApp/WeatherHandler.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.XML;
 
@@ -28,16 +29,21 @@ public class WeatherHandler {
 	}
 
 
-	public void extractWeatherInformation() throws IOException {
-		jsonObject = XML.toJSONObject(getJsonString(weatherUrl));
+	private void extractWeatherInformation() throws IOException {
+		try {
+			jsonObject = XML.toJSONObject(getJsonString(weatherUrl));
 
-		JSONObject todayJson = jsonObject.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time").getJSONObject(16);
-		JSONObject tomorrowJson = jsonObject.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time").getJSONObject(136);
+			JSONObject todayJson = jsonObject.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time").getJSONObject(16);
+			JSONObject tomorrowJson = jsonObject.getJSONObject("weatherdata").getJSONObject("product").getJSONArray("time").getJSONObject(136);
 
-		JSONObject tempJsonObject = todayJson.getJSONObject("location").getJSONObject("symbol");
-		JSONObject tempJsonObject2 = tomorrowJson.getJSONObject("location").getJSONObject("symbol");
-		todaySymbolNumber = tempJsonObject.get("number").toString();
-		tomorrowSymbolNumber = tempJsonObject2.get("number").toString();
+			JSONObject tempJsonObject = todayJson.getJSONObject("location").getJSONObject("symbol");
+			JSONObject tempJsonObject2 = tomorrowJson.getJSONObject("location").getJSONObject("symbol");
+			todaySymbolNumber = tempJsonObject.get("number").toString();
+			tomorrowSymbolNumber = tempJsonObject2.get("number").toString();
+		}
+		catch(JSONException e){
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public String getTodaySymbolNumber() {
@@ -48,7 +54,7 @@ public class WeatherHandler {
 		return tomorrowSymbolNumber;
 	}
 
-	public String getJsonString(String url) throws IOException {
+	private String getJsonString(String url) throws IOException {
 		URLConnection connection = new URL(url).openConnection();
 		connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
 		connection.connect();
@@ -64,11 +70,12 @@ public class WeatherHandler {
 		return stringBuilder.toString();
 	}
 
-	public void setWeatherUrl(String latitude, String longitude) {
+	private void setWeatherUrl(String latitude, String longitude) {
 		this.weatherUrl = "https://api.met.no/weatherapi/locationforecast/1.9/?lat=" +
 				latitude + "&lon=" + longitude;
 	}
 
+	/*
 	public String getGlobalLat() {
 		return globalLat;
 	}
@@ -76,4 +83,5 @@ public class WeatherHandler {
 	public String getGlobalLong() {
 		return globalLong;
 	}
+	*/
 }
